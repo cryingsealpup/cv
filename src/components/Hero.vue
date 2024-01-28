8ade01ffe672e6157a586bbb8f40f00d86eb6327
Ekaterina Radchenko <ekatherinered@gmail.com>
2024-01-29 03:13:18 +0400

    init: setup project

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+<template>
+    <div class="header__wrapper">
+        <h1>
+            Екатерина Радченко
+        </h1>
+        <h2>
+            <ul class="header__more-info">
+                <li>
+                    Frontend-разработчик
+                </li>
+                <li>
+                    Vanilla JS / Vue
+                </li>
+            </ul>
+        </h2>
+    </div>
+</template>
